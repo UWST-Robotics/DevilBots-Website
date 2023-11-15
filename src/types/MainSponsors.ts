7d65bcaf21e71785c88f74e023680705e7931008
Digi <aus343@gmail.com>
2023-11-14 18:57:45 -0600

    Added Phillips Medisize

--- src/types/MainSponsors.ts
+++ src/types/MainSponsors.ts
@@ -9,6 +9,7 @@ export const OtherSponsors: Sponsor[] = [];
 const MainSponsors: Sponsor[] = [
     { id: "Coex3D", href: "https://coex3d.com/" },
     { id: "Tek Pak", href: "https://www.tekpak.com/" },
-    { id: "IMS", href: "https://innmacspe.com/" }
+    { id: "IMS", href: "https://innmacspe.com/" },
+    { id: "Phillips-Medisize", href: "https://www.phillipsmedisize.com/" }
 ];
 export default MainSponsors;
