8a10aa4ba2ca304c286e238abdb4b5db69e1d452
Digi <aus343@gmail.com>
2023-12-02 19:42:27 -0600

    Added Shape Products & Stout Bread

--- src/types/MainSponsors.ts
+++ src/types/MainSponsors.ts
@@ -10,6 +10,8 @@ const MainSponsors: Sponsor[] = [
     { id: "Coex3D", href: "https://coex3d.com/" },
     { id: "Tek Pak", href: "https://www.tekpak.com/" },
     { id: "IMS", href: "https://innmacspe.com/" },
-    { id: "Phillips-Medisize", href: "https://www.phillipsmedisize.com/" }
+    { id: "Phillips-Medisize", href: "https://www.phillipsmedisize.com/" },
+    { id: "Shape", href: "https://shapeproducts.com/" },
+    { id: "StoutBread", href: "https://stoutbread.com/" }
 ];
 export default MainSponsors;
